f4fd8fb106d651a11a341ba7ea77cc5bb8dd2282
Amrul Izwan <amrulizwan7@gmail.com>
2024-11-18 05:45:55 +0800

    Update MIddleware in Routes

--- backend/routes/testimoniRoutes.js
+++ backend/routes/testimoniRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { protect } = require('../middlewares/authMiddleware');
+const { protect, admin } = require('../middlewares/authMiddleware');
 const { addTestimoni, getTestimoni, updateTestimoni, deleteTestimoni } = require('../controllers/testimoniController');
 
 const router = express.Router();
 
 router.get('/', getTestimoni);
-router.post('/', protect, addTestimoni);
-router.put('/:id', protect, updateTestimoni);
-router.delete('/:id', protect, deleteTestimoni);
+router.post('/', protect, admin, addTestimoni);
+router.put('/:id', protect, admin, updateTestimoni);
+router.delete('/:id', protect, admin, deleteTestimoni);
 
 module.exports = router;
