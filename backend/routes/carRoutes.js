dd3a5b234a42d82c9d1f69b76924f348183b3c98
Amrul Izwan <amrulizwan7@gmail.com>
2024-11-21 13:41:07 +0800

    update routes add middleware

--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const { protect, admin } = require('../middlewares/authMiddleware');
 const { addCar, getCars, getCarById, updateCar, deleteCar } = require('../controllers/carController');
+const upload = require('../middlewares/upload');
 
 const router = express.Router();
 
 router.get('/', getCars);
 router.get('/:id', getCarById);
-router.post('/', protect, admin, addCar);
-router.put('/:id', protect, admin, updateCar);
+router.post('/', protect, admin, upload.single('image'), addCar);
+router.put('/:id', protect, admin, upload.single('image'), updateCar);
 router.delete('/:id', protect, admin, deleteCar);
 
 module.exports = router;
