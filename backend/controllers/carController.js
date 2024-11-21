d75e157b5c4c7439ec4e81c5cbf7ffba79c20df0
Amrul Izwan <amrulizwan7@gmail.com>
2024-11-21 22:24:30 +0800

    Update Controller

--- backend/controllers/carController.js
+++ backend/controllers/carController.js
@@ -1,4 +1,5 @@
 const Car = require('../models/Car');
+const fs = require('fs');
 
 exports.addCar = async (req, res) => {
   try {
@@ -43,6 +44,11 @@ exports.updateCar = async (req, res) => {
     if (req.file) {
       updates.image = req.file.path;
     }
+
+    const carImage = await Car.findById(id).select('image');
+    if (carImage) {
+      fs.unlinkSync(carImage.image);
+    }
     const car = await Car.findByIdAndUpdate(id, updates, { new: true });
     if (car) {
       res.json(car);
@@ -60,6 +66,9 @@ exports.deleteCar = async (req, res) => {
 
     const car = await Car.findByIdAndDelete(id);
     if (car) {
+      if (car.image) {
+        fs.unlinkSync(car.image);
+      }
       res.json({ message: 'Car deleted' });
     } else {
       res.status(404).json({ message: 'Car not found' });
