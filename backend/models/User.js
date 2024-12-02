db46efe7fde2b5db36a260631c16d9401cb309f8
Amrul Izwan <amrulizwan7@gmail.com>
2024-12-02 19:12:43 +0800

    edit User Models

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   role: { type: String, enum: ["admin", "user"], default: "admin" },
   token: { type: String },
+  resetToken: { type: String },
+  resetTokenExpiry: { type: Date },
 });
 
 userSchema.pre("save", async function (next) {
