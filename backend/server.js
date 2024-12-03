d725b4e6da49ff1e4a24501e3693b18a5a6d850d
ayuistawati <istawatiayu51@gmail.com>
2024-12-03 13:49:41 +0800

    update server + package

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const connectDB = require('./configs/db');
 const userRoutes = require('./routes/userRoutes');
 const carRoutes = require('./routes/carRoutes');
 const testimoniRoutes = require('./routes/testimoniRoutes');
+const blogRoutes = require("./routes/blogRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const orderRoutes = require("./routes/orderRoutes"); 
+const subscriberRoutes = require("./routes/subscriberRoutes"); 
 
 connectDB();
 
@@ -13,6 +17,11 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/cars', carRoutes);
 app.use('/api/testimoni', testimoniRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/orders", orderRoutes); 
+app.use("/api/subscriber", subscriberRoutes); 
+
 
 const PORT = process.env.PORT || 5000;
 
