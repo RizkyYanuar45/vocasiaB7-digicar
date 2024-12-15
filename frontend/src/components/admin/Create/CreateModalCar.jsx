6e634b8cebf5c96f4925854eff6a70bcaf72861f
Amrul Izwan <amrulizwan7@gmail.com>
2024-12-15 09:21:40 -0600

    Update Modal

--- frontend/src/components/admin/Create/CreateModalCar.jsx
+++ frontend/src/components/admin/Create/CreateModalCar.jsx
@@ -1,11 +1,14 @@
-function CreateModal({ isOpen, onClose }) {
+function CreateModal({ isOpen, onClose, onCreate }) {
   if (!isOpen) return null;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const newCar = Object.fromEntries(formData);
+    onCreate(newCar);
+  };
 
   return (
-    <div
-      className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen bg-black bg-opacity-50"
-      aria-hidden="true"
-    >
+    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen bg-black bg-opacity-50" aria-hidden="true">
       <div className="relative p-4 w-full max-w-md">
         {/* Modal content */}
         <div className="relative bg-secondary rounded-lg shadow">
@@ -17,33 +20,18 @@ function CreateModal({ isOpen, onClose }) {
               type="button"
               className="text-text bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center"
             >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
           </div>
 
           {/* Modal body */}
-          <form className="p-4 md:p-5">
+          <form className="p-4 md:p-5" onSubmit={handleSubmit}>
             <div className="grid gap-4 mb-4 grid-cols-2">
               <div className="col-span-2">
-                <label
-                  htmlFor="name"
-                  className="block mb-2 text-sm font-medium text-text"
-                >
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-text">
                   Car Name
                 </label>
                 <input
@@ -56,15 +44,12 @@ function CreateModal({ isOpen, onClose }) {
                 />
               </div>
               <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="price"
-                  className="block mb-2 text-sm font-medium text-text"
-                >
+                <label htmlFor="price" className="block mb-2 text-sm font-medium text-text">
                   Price Per Day
                 </label>
                 <input
                   type="number"
-                  name="price"
+                  name="pricePerDay"
                   id="price"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   placeholder="ex. 500000"
@@ -72,68 +57,34 @@ function CreateModal({ isOpen, onClose }) {
                 />
               </div>
               <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="year"
-                  className="block mb-2 text-sm font-medium text-text"
-                >
+                <label htmlFor="year" className="block mb-2 text-sm font-medium text-text">
                   Car Year
                 </label>
+                <input type="number" name="tahun" id="year" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="ex. 2014" required />
+              </div>
+              <div className="col-span-2">
+                <label htmlFor="description" className="block mb-2 text-sm font-medium text-text">
+                  Description
+                </label>
                 <input
-                  type="number"
-                  name="year"
-                  id="year"
+                  type="text"
+                  name="description"
+                  id="description"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                  placeholder="ex. 2014"
+                  placeholder="Type car description"
                   required
                 />
               </div>
-              <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="category"
-                  className="block mb-2 text-sm font-medium text-text"
-                >
-                  Plat Type
-                </label>
-                <select
-                  id="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                >
-                  <option defaultValue="">Select Type</option>
-                  <option value="Ganjil">Ganjil</option>
-                  <option value="Genap">Genap</option>
-                </select>
-              </div>
               <div className="col-span-2">
-                <label
-                  htmlFor="image"
-                  className="block mb-2 text-sm font-medium text-text"
-                >
+                <label htmlFor="image" className="block mb-2 text-sm font-medium text-text">
                   Upload Image
                 </label>
-                <input
-                  type="file"
-                  id="image"
-                  name="image"
-                  accept="image/*"
-                  className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none"
-                />
+                <input type="file" id="image" name="image" accept="image/*" className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer focus:outline-none" />
               </div>
             </div>
-            <button
-              type="submit"
-              className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-              <svg
-                className="me-1 -ms-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clipRule="evenodd"
-                />
+            <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+              <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
               </svg>
               Add new car
             </button>
